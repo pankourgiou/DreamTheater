eef8465bd9ca372e96df9cfd204848738a9cc04f
pankourgiou <60981745+pankourgiou@users.noreply.github.com>
2021-10-31 09:27:36 +0200

    Create Dt.py

---
+++ Dt.py
@@ -0,0 +1,12 @@
+import time
+
+
+t1 = time.time()
+
+for _ in range(2000):
+    print("Reevaluation of Dream Theater band album artwork and booklets as NFTs")
+
+
+t2 = time.time()
+t = t2 - t1
+print("Elapsed time is : ", t, " seconds")
